8e0ea01f8a8a353b81cf757bfd2f5a6a620001dc
Eudald <eudald@correig.net>
2018-02-26 14:32:17 +0100

    Actaulització 26 de febrer

--- classe_1_Intro.R
+++ classe_1_Intro.R
@@ -17,7 +17,6 @@
 # La part de test d'hipòtesi no la cobreixen, aquesta està basada en el llibre:
 # Statistics, An introduction using R, de Michael J. Crawley i que podeu trobar a la biblio
 
-
 # Tant en els llibres com en els vídeos hi ha moltíssima més informació de la que farem 
 # en el curs i és un lloc fantàstic pels que vulguin ampliar.
 
@@ -29,10 +28,13 @@
 # Tutorials: https://www.statmethods.net/index.html, https://www.r-bloggers.com
 # Documentació: https://cran.r-project.org/manuals.html
 # LLibreries: exemple: https://cran.r-project.org/web/packages/lme4/lme4.pdf
-# Demanar ajuda: https://stackoverflow.com (prgramar en R)
+# Demanar ajuda: https://stackoverflow.com (prgramar)
 # Demanar ajuda: https://stats.stackexchange.com/ (estadística)
 
 # Per qualsevol altra cosa, google.
+
+# Aquesta classe la podeu trobar actualitzada a 
+# https://github.com/ecorreig/Curs-R
 
 
     #####################################
@@ -48,6 +50,8 @@ a = 3
 x = c(1,9,3,4) # c ve de concatenar, és de les opeacions més útils
 y = seq(0,1,100) # creem un vector a través de l'operació seqüència
 ?seq #mirem què és això de l'opearció seqüència
+
+# El signe = és totalment equivalent a <- en R
 
 y = seq(from=0, to=1, length = 100)
 
@@ -185,8 +189,12 @@ liver = read.csv('indian_cast.csv', sep=";", dec = ",")
 
 View(liver) # també es pot clicar al símbol blau al costat del nom a l'environment
 
-# This data set contains 416 liver patient records and 167 non liver patient records collected from North East of Andhra Pradesh, India.
-# The "Dataset" column is a class label used to divide groups into liver patient (liver disease) or not (no disease). 
+# Només a tall informatiu, què hi ha en la base de dades que utilitzarem 
+
+# This data set contains 416 liver patient records and 167 non liver patient records 
+# collected from North East of Andhra Pradesh, India.
+# The "Dataset" column is a class label used to divide groups into liver patient 
+# (liver disease) or not (no disease). 
 # This data set contains 441 male patient records and 142 female patient records.
 # 
 # Any patient whose age exceeded 89 is listed as being of age "90".
@@ -203,22 +211,35 @@ View(liver) # també es pot clicar al símbol blau al costat del nom a l'environ
 # Total Protiens
 # Albumin
 # Albumin and Globulin Ratio
-# Dataset: field used to split the data into two sets (patient with liver disease, or no disease)
+# Dataset: field used to split the data into two sets (patient with liver disease, 
+# or no disease)
+
+#### Informació sobre la base de dades ####
 
 colnames(liver)
 dim(liver)
 class(liver)
 str(liver)
 
-liver$Dataset
-liver[,c(1,3)]
-head(liver[,c(1,3)])
+summary(liver) # molt útil per tenir una visió general
+pairs(liver) # o plot(liver) també per tenir una visió general
 
+liver$Dataset # accedir a una columna (variable) concreta
+liver[,c(1,3)] # accedir a diverses columnes
+
+head(liver) # 5 primeres files -> va bé per tenir una visió ràpida de com són els valors
+tail(liver) # 5 últimes files
+
+# Podem combinar coses:
+head(liver[,c(1,3)])
 tail(liver[,c("Age","Dataset")])
 
 liver[1:10,c("Age","Dataset")]
 
-# canviar tipus de variable, funcions as.factor, as.numeric, as.character
+# canviar tipus de variable, funcions: 
+# as.factor -> passar a categòrica (R crea els "dummies" automàticament!)
+# as.numeric -> passar a numèrica 
+# as.character -> passar a caràcter (CIPs dels pacients, per exmeple)
 
 liver$Dataset = as.factor(liver$Dataset)
 
@@ -226,7 +247,18 @@ str(liver)
 
 # liver$Dataset = as.numeric(as.character(liver$Dataset)) # a vegades s'ha de fer així
 
+# si volem canviar el tipus de moltes columnes:
+
+cols = c(1,5,6,7) # cols = c(1,5:7)
+
+for (i in cols){
+  liver[,i] = as.numeric(liver[,i])
+}
+
+str(liver)
 summary(liver)
+
+#### Attach, detach ####
 
 plot(Age, Albumin_and_Globulin_Ratio) # error
 
@@ -249,22 +281,64 @@ hist(Age)
 detach(liver)
 
 
-# manipulació de dades
+#### Manipulació de dades i "subsetting" ####
 
+# creem una nova columna a partir de les anteriors
 liver$prova = liver$prova+liver$Total_Bilirubin
 
+# creem una nova columna categòrica amb l'ajuda de la funció ifelse()
+# per a més info ?ifelse
+
+# dues categories:
+liver$cat = ifelse(liver$prova>5,1,0)
+
+# tres categories:
 liver$bins = ifelse(liver$prova>5,1,ifelse(liver$prova>2,2,3))
 
+# podem anar concatenant ifelse per tenir tantes categories com vulguem
+
+# Eliminem una columna; es pot fer de dues maneres:
 liver = liver[,-12]
 liver$bins = NULL
 
+# Seguim creant columnes fent opearcions amb altres columnes
 liver$prova = liver$Direct_Bilirubin*2.3
 
-liver_dones = liver[liver$Gender=="Female",] # alerta amb la coma!!!
+# Subsetting 
+
+# Treballem la part d'agafar subgrups del data frame complet (seria l'equivalent del filtre de l'SPSS, 
+# amb la diferència que en R normalment creem una dataframe nova cada vegada que fem algun filtrat).
+# El subsetting és una de les parts que costa més al principi; una bona referència/resum es pot trobar a
+# https://www.statmethods.net/management/subset.html
+# Alerta amb corxets, parèntisis, comes; tot ha d'estar exactament al seu lloc
+
+# Com funciona:
+
+# L'R escull files (o columnes) basat en booleans, és a dir, en columnes de veritat i false; 
+# què vol dir això:
+
+liver$Gender=="Female"
+
+# Aquí veiem que, de totes les files del dataframe liver, ens surt un True si en aquella fila la 
+# variable Gender és igual a "Female" i un False si no ho és.
+
+# Si això ho poso en la següent estructura, estaré agafant només aquelles files on hi hagi un True
+# en la condició que explicito dins dels corxets.
+
+liver_dones = liver[liver$Gender=="Female",] # alerta amb la coma! 
+                                            # -> La coma vol dir que volem agafar totes les columnes
+
+# Aquí el mateix, tindrem un True per totles les files que tinguin la bilirrubina a més de 5 i 
+# un False a les altres; per tant estarem creant una nova base de dades amb pacients amb la 
+# bilirrubina més alta de 5. 
 
 liver_bili = liver[liver$Total_Bilirubin>5,]
 
-liver_bili = liver[(liver$Total_Bilirubin>1 &liver$Total_Bilirubin<5),]
+# Podem incloure dues condicions amb el signe & (and) o | (or)
+
+liver_bili = liver[(liver$Total_Bilirubin>1 & liver$Total_Bilirubin<5),]
+
+# Més exemples:
 
 colnames(liver)[ncol(liver)-1] = "malaltia"
 
@@ -272,13 +346,5 @@ levels(liver$malaltia) = c("no malalt", "malalt")
 
 liver_malalt = liver[liver$malaltia=="malalt",]
 
-# si volem canviar el tipus de moltes columnes:
 
-cols = c(1,5,6,7) # cols = c(1,5:7)
-
-for (i in cols){
-  liver[,i] = as.numeric(liver[,i])
-}
-
-str(liver)
 
